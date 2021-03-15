22a89e7d6b1609eb1841434c1d2740c692e51cda
Frank Benicio <frank.benicio@infouniron.com>
2021-03-14 20:39:01 -0400

    ensure RemoveAuthentication calls HttpClient with corrent Url

---
+++ test/data/usecases/remote_authentication_test.dart
@@ -0,0 +1,40 @@
+
+import 'package:faker/faker.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:meta/meta.dart';
+
+class RemoteAuthentication{
+
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({@required this.httpClient, @required this.url});
+
+  Future<void> auth() async{
+    await httpClient.request(url: url);
+  }
+
+}
+
+abstract class HttpClient{
+  Future<void> request({@required String url});
+}
+
+class HttpClientSpy extends Mock implements HttpClient{}
+
+
+
+void main(){
+  test("Should call HttpCliente with corrent URL", () async {
+    final httpClient = HttpClientSpy();
+    final url = faker.internet.httpUrl();
+    final sut = RemoteAuthentication(httpClient: httpClient, url: url);
+
+    await sut.auth();
+
+
+    verify(httpClient.request(url: url));
+
+  });
+}
