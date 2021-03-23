10082783a0952ce0c0cdc3593699c0524891a1c6
Frank Benicio <frank.benicio@infouniron.com>
2021-03-23 19:50:41 -0400

    ensure HttpAdapter calls post without Body

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -18,7 +18,10 @@ class HttpAdapter {
       'content-type': 'application/json',
       'accept': 'application/json',
     };
-    await client.post(url, headers: headers, body: jsonEncode(body));
+
+    final jsonBody = body != null ? jsonEncode(body): null;
+
+    await client.post(url, headers: headers, body: jsonBody);
   }
 }
 
@@ -46,6 +49,16 @@ void main() {
           'accept': 'application/json',
         },
         body: '{"any_key":"any_value"}'
+      ));
+    });
+
+    test('Should call post  without body', () async {
+
+      await sut.request(url: url, method: 'post');
+
+      verify(client.post(
+          any,
+          headers: anyNamed('headers'),
       ));
     });
   });
