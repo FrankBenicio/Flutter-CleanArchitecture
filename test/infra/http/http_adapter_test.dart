cb2758c71a6c57e88729654f52f365db0d579f5c
Frank Benicio <frank.benicio@infouniron.com>
2021-03-23 19:42:33 -0400

    move sut creation to the setup method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -21,11 +21,19 @@ class HttpAdapter {
 }
 
 void main() {
+  HttpAdapter sut;
+  ClientSpy client;
+  String url;
+  
+  setUp((){
+    client = ClientSpy();
+    sut = HttpAdapter(client);
+    url = faker.internet.httpUrl();
+  });
+  
+  
   group('post', () {
     test('Should call post with values', () async {
-      final client = ClientSpy();
-      final sut = HttpAdapter(client);
-      final url = faker.internet.httpUrl();
 
       await sut.request(url: url, method: 'post');
 
