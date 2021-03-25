5a352f34bfcd67cc7cb7693a472e156692cb73f6
Frank Benicio <frank.benicio@infouniron.com>
2021-03-25 00:37:22 -0400

    ensure HttpAdapter returns null if post returns 204

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -25,7 +25,10 @@ class HttpAdapter implements HttpClient {
 
     final response = await client.post(url, headers: headers, body: jsonBody);
 
-    return response.body.isEmpty ? null : jsonDecode(response.body);
+    if(response.statusCode == 200)
+      return response.body.isEmpty ? null : jsonDecode(response.body);
+    else
+      return null;
   }
 }
 
@@ -89,5 +92,22 @@ void main() {
 
       expect(response, null);
     });
+
+    test('Should return null if post returns 204', () async {
+      mockResponse(204, body: "");
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
+    test('Should return null if post returns 204 with data', () async {
+      mockResponse(204);
+
+      final response = await sut.request(url: url, method: 'post');
+
+      expect(response, null);
+    });
+
   });
 }
