d31a3bb24b5eca0059f8f146f58ce8c1dcfdf043
Frank Benicio <frank.benicio@infouniron.com>
2021-03-24 21:11:25 -0400

    move duplicated code to the setup method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -41,11 +41,19 @@ void main() {
   });
 
   group('post', () {
-    test('Should call post with values', () async {
-      when(client.post(any,
-              body: anyNamed('body'), headers: anyNamed('headers')))
-          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
+    PostExpectation mockRequest() => when(
+        client.post(any, body: anyNamed('body'), headers: anyNamed('headers')));
 
+    void mockResponse(int statusCode,
+        {String body = '{"any_key":"any_value"}'}) {
+      mockRequest().thenAnswer((_) async => Response(body, statusCode));
+    }
+
+    setUp(() {
+      mockResponse(200);
+    });
+
+    test('Should call post with values', () async {
       await sut
           .request(url: url, method: 'post', body: {'any_key': 'any_value'});
 
@@ -58,9 +66,6 @@ void main() {
     });
 
     test('Should call post  without body', () async {
-      when(client.post(any,
-              body: anyNamed('body'), headers: anyNamed('headers')))
-          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
 
       await sut.request(url: url, method: 'post');
 
@@ -71,8 +76,6 @@ void main() {
     });
 
     test('Should return data if post returns 200', () async {
-      when(client.post(any, headers: anyNamed('headers')))
-          .thenAnswer((_) async => Response('{"any_key":"any_value"}', 200));
 
       final response = await sut.request(url: url, method: 'post');
 
@@ -80,8 +83,7 @@ void main() {
     });
 
     test('Should return null if post returns 200 with no data', () async {
-      when(client.post(any, headers: anyNamed('headers')))
-          .thenAnswer((_) async => Response('', 200));
+      mockResponse(200, body: "");
 
       final response = await sut.request(url: url, method: 'post');
 
