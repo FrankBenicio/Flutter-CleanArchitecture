a00258352d007a3e2855ccf32b3f2937c0f2c9de
Frank Benicio <frank.benicio@infouniron.com>
2021-03-26 20:40:21 -0400

    move duplicated code to a private method

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -1,36 +1,11 @@
-import 'dart:convert';
 
-import 'package:app/data/http/http.dart';
+import 'package:app/infra/http/http.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart';
 import 'package:mockito/mockito.dart';
-import 'package:meta/meta.dart';
 
 class ClientSpy extends Mock implements Client {}
-
-class HttpAdapter implements HttpClient {
-  final Client client;
-
-  HttpAdapter(this.client);
-
-  Future<Map> request(
-      {@required String url, @required String method, Map body}) async {
-    final headers = {
-      'content-type': 'application/json',
-      'accept': 'application/json',
-    };
-
-    final jsonBody = body != null ? jsonEncode(body) : null;
-
-    final response = await client.post(url, headers: headers, body: jsonBody);
-
-    if(response.statusCode == 200)
-      return response.body.isEmpty ? null : jsonDecode(response.body);
-    else
-      return null;
-  }
-}
 
 void main() {
   HttpAdapter sut;
