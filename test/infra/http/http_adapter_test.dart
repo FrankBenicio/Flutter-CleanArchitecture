d002b9daacd239039e7b962eb6f09b2572bbba91
Frank Benicio <frank.benicio@infouniron.com>
2021-03-26 21:10:25 -0400

    ensure httpAdapter throws ServerError method is provided

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -22,7 +22,7 @@ void main() {
 
   group('shared', (){
     test('Should throw ServerError if invalid method is provided', () async {
-   final future =   await sut
+   final future =   sut
           .request(url: url, method: 'invalid_method');
 
      expect(future, throwsA(HttpError.serverError));
