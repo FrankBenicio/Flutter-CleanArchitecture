7d53a203f2aa7a6c42d0cb3c140f77c7260fc514
Frank Benicio <frank.benicio@infouniron.com>
2021-03-26 20:46:55 -0400

    ensure httpAdapter throws BadRequestError on 400 no data

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -94,5 +94,13 @@ void main() {
       expect(future, throwsA(HttpError.badRequest));
     });
 
+    test('Should return BadRequest if post returns 400', () async {
+      mockResponse(400, body: '');
+
+      final future  =  sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.badRequest));
+    });
+
   });
 }
