edc63e1b860e9ef40807688a0ae1a046818881e8
Frank Benicio <frank.benicio@infouniron.com>
2021-03-26 21:14:03 -0400

    ensure httpAdapter throws ServerError if post throws

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -38,6 +38,10 @@ void main() {
       mockRequest().thenAnswer((_) async => Response(body, statusCode));
     }
 
+    void mockError() {
+      mockRequest().thenThrow(Exception());
+    }
+
     setUp(() {
       mockResponse(200);
     });
@@ -142,6 +146,15 @@ void main() {
       final future  =  sut.request(url: url, method: 'post');
 
       expect(future, throwsA(HttpError.notFound));
+    });
+
+
+    test('Should return ServerError if post throws', () async {
+      mockError();
+
+      final future  =  sut.request(url: url, method: 'post');
+
+      expect(future, throwsA(HttpError.serverError));
     });
 
   });
