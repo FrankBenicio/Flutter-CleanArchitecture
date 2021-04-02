5155947d0d62a3062930f37b6f4a9870d5d1076f
Frank Benicio <frank.benicio@infouniron.com>
2021-04-01 23:02:35 -0400

    ensure LoginPage presents error if password is valid

--- test/ui/pages/login_page_test.dart
+++ test/ui/pages/login_page_test.dart
@@ -131,4 +131,32 @@ void main() {
 
         expect(find.text('any error'), findsOneWidget);
       });
+
+  testWidgets('Should present error if password is valid',
+          (WidgetTester tester) async {
+        await loadPage(tester);
+
+        passwordErrorController.add(null);
+
+        await tester.pump();
+
+        final passwordTextChield = find.descendant(
+            of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+
+        expect(passwordTextChield, findsOneWidget);
+      });
+
+  testWidgets('Should present error if password is valid',
+          (WidgetTester tester) async {
+        await loadPage(tester);
+
+        passwordErrorController.add('');
+
+        await tester.pump();
+
+        final passwordTextChield = find.descendant(
+            of: find.bySemanticsLabel('Senha'), matching: find.byType(Text));
+
+        expect(passwordTextChield, findsOneWidget);
+      });
 }
