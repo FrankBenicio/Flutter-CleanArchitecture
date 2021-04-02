20a7b90f90c51158263a00fea2f69f12804476e0
Frank Benicio <frank.benicio@infouniron.com>
2021-04-02 12:29:32 -0400

    move LoginButton to its own file

--- lib/ui/pages/login/login_page.dart
+++ lib/ui/pages/login/login_page.dart
@@ -54,16 +54,7 @@ class _LoginPageState extends State<LoginPage> {
                             padding: EdgeInsets.only(top: 8, bottom: 32),
                             child: PasswordInput(),
                           ),
-                          StreamBuilder<bool>(
-                              stream: widget.presenter.isFormValidStream,
-                              builder: (context, snapshot) {
-                                return RaisedButton(
-                                  onPressed: snapshot.data == true
-                                      ? widget.presenter.auth
-                                      : null,
-                                  child: Text('Entrar'.toUpperCase()),
-                                );
-                              }),
+                          LoginButton(),
                           FlatButton.icon(
                             onPressed: () {},
                             icon: Icon(Icons.person),
@@ -88,6 +79,8 @@ class _LoginPageState extends State<LoginPage> {
     widget.presenter.dispose();
   }
 }
+
+
 
 
 
