5ac2f2c302d8cebc4011bbf3abcdfb5d1155c286
Frank Benicio <frank.benicio@infouniron.com>
2021-04-03 15:38:28 -0400

    move Authentication mock to a helper method

--- test/presentation/presenters/stream_login_presenter_test.dart
+++ test/presentation/presenters/stream_login_presenter_test.dart
@@ -9,7 +9,7 @@ abstract class Validation {
   String validate({@required String field, @required String value});
 }
 
-class LoginState{
+class LoginState {
   String emailError;
 }
 
@@ -20,13 +20,14 @@ class StreamLoginPresenter {
 
   final _state = LoginState();
 
-  Stream<String> get emailErrorStream => _controller.stream.map((state) => state.emailError);
+  Stream<String> get emailErrorStream =>
+      _controller.stream.map((state) => state.emailError);
 
   StreamLoginPresenter({@required this.validation});
 
   void validateEmail(String email) {
-   _state.emailError = validation.validate(field: 'email', value: email);
-   _controller.add(_state);
+    _state.emailError = validation.validate(field: 'email', value: email);
+    _controller.add(_state);
   }
 }
 
@@ -37,12 +38,22 @@ void main() {
   ValidationSpy validation;
   String email;
 
+  PostExpectation mockValidationCall(String field) => when(validation.validate(
+      field: field == null ? anyNamed('field') : field,
+      value: anyNamed('value')));
+
+  void mockValidation({String field, String value}) {
+    mockValidationCall(field).thenReturn(value);
+  }
+
   setUp(() {
     validation = ValidationSpy();
 
     sut = StreamLoginPresenter(validation: validation);
 
     email = faker.internet.email();
+
+    mockValidation();
   });
 
   test('Should call validation with correct email', () {
@@ -52,12 +63,10 @@ void main() {
   });
 
   test('Should emit email error if validation fails', () {
-    when(validation.validate(field: anyNamed('field'), value: anyNamed('value')))
-    .thenReturn('error');
+    mockValidation(value: 'error');
 
     expectLater(sut.emailErrorStream, emits('error'));
 
     sut.validateEmail(email);
-
   });
 }
