ad506b705afacc28dc667c9d35962729eb26a2bd
Frank Benicio <frank.benicio@infouniron.com>
2021-04-06 01:13:57 -0400

    ensure LocalStorageAdapter throws if save secure throws

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -26,7 +26,12 @@ void main() {
 
   String value;
 
-  setUp((){
+  void mockSaveSecureError(){
+    when(secureStorage.write(key: anyNamed('key'), value: anyNamed('value')))
+        .thenThrow(Exception());
+  }
+
+  setUp(() {
     secureStorage = FlutterSecureStorageSpy();
 
     sut = LocalStorageAdapter(secureStorage: secureStorage);
@@ -39,5 +44,13 @@ void main() {
     await sut.saveSecure(key: key, value: value);
 
     verify(secureStorage.write(key: key, value: value));
+  });
+
+  test('Should throw if save secure throws', () async {
+    mockSaveSecureError();
+
+    final future = sut.saveSecure(key: key, value: value);
+
+    expect(future, throwsA(TypeMatcher<Exception>()));
   });
 }
