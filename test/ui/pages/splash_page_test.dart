e86c4a04764136290a621b0895e7c9c567b7ea85
Frank Benicio <frank.benicio@infouniron.com>
2021-04-06 23:21:02 -0400

    ensure SplashPage presents spinner on page load

---
+++ test/ui/pages/splash_page_test.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('4Dev')),
+      body: Center(
+        child: CircularProgressIndicator(),
+      ),
+    );
+  }
+}
+
+void main() {
+  Future loadPage(WidgetTester tester) async {
+    await tester.pumpWidget(GetMaterialApp(
+      initialRoute: '/',
+      getPages: [
+        GetPage(name: '/', page: () => SplashPage()),
+      ],
+    ));
+  }
+
+  testWidgets('Should present spinner on page load',
+      (WidgetTester tester) async {
+    //
+    await loadPage(tester);
+
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+  });
+
+  testWidgets('Should call loadCurrentAccount on page load',
+          (WidgetTester tester) async {
+        //
+        await loadPage(tester);
+
+        expect(find.byType(CircularProgressIndicator), findsOneWidget);
+      });
+}
