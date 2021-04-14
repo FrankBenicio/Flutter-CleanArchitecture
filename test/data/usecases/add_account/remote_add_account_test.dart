57ffcbf5a56c49aeaacc0a1972d8a361a22398b7
Frank Benicio <frank.benicio@infouniron.com>
2021-04-14 12:33:47 -0400

    ensure RemoteAddAccount throws EmaInUseError on 403

--- test/data/usecases/add_account/remote_add_account_test.dart
+++ test/data/usecases/add_account/remote_add_account_test.dart
@@ -74,4 +74,12 @@ void main() {
     expect(future, throwsA(DomainError.unexpected));
   });
 
+  test("Should throw EmailInUse if HttpClient returns 403", () async {
+    mockHttpError(HttpError.forbidden);
+
+    final future = sut.add(params);
+
+    expect(future, throwsA(DomainError.emailInUse));
+  });
+
 }
