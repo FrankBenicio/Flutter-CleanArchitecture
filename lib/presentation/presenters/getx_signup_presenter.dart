6cb2ab2ff7f74824892542e19cd9900536035bb8
Frank Benicio <frank.benicio@infouniron.com>
2021-05-31 21:46:36 -0400

    ensure GetXSignUpPresenter emits error if AddAccount fails

--- lib/presentation/presenters/getx_signup_presenter.dart
+++ lib/presentation/presenters/getx_signup_presenter.dart
@@ -104,8 +104,8 @@ class GetXSignUpPresenter extends GetxController {
     await saveCurrentAccount.save(account);
     } on DomainError catch (error) {
       switch (error) {
-        case DomainError.invalidCredentials:
-          _mainError.value = UIError.invalidCredentials;
+        case DomainError.emailInUse:
+          _mainError.value = UIError.emailInUse;
           break;
         default:
           _mainError.value = UIError.unexpected;
