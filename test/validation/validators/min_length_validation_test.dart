983c94e9339558316219a5d2a5b5b5c651f64814
Frank Benicio <frank.benicio@infouniron.com>
2021-06-21 22:38:42 -0400

    ensure MinLengthValidation returns null if value is equal than min size

---
+++ test/validation/validators/min_length_validation_test.dart
@@ -0,0 +1,52 @@
+import 'package:ForDev/presentation/protocols/validation.dart';
+import 'package:ForDev/validation/protocols/protocols.dart';
+import 'package:faker/faker.dart';
+import 'package:test/test.dart';
+import 'package:meta/meta.dart';
+
+class MingLengthValidation implements FieldValidation {
+  final String field;
+  final int size;
+
+  MingLengthValidation({@required this.field, @required this.size});
+
+  ValidationError validate(String value) {
+    return value?.length == size ? null : ValidationError.invalidField;
+  }
+}
+
+void main() {
+  MingLengthValidation sut;
+  
+  setUp((){
+     sut = MingLengthValidation(field: 'any_field', size: 5);
+  });
+  test('Should retorn error if value is empty', () {
+    
+    final error = sut.validate('');
+
+    expect(error, ValidationError.invalidField);
+  });
+
+
+  test('Should retorn error if value is null', () {
+
+    final error = sut.validate(null);
+
+    expect(error, ValidationError.invalidField);
+  });
+
+  test('Should retorn error if value is less than min size', () {
+
+    final error = sut.validate(faker.randomGenerator.string(4, min: 1));
+
+    expect(error, ValidationError.invalidField);
+  });
+
+  test('Should retorn null if value is equal than min size', () {
+
+    final error = sut.validate(faker.randomGenerator.string(5, min: 5));
+
+    expect(error, null);
+  });
+}
