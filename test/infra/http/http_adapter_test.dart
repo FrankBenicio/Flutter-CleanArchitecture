62ac88dc5da6512b5ac954eab6d2bddfe283a89c
Frank Benicio <frank.benicio@infouniron.com>
2021-12-20 22:14:17 -0400

    ensure HttpAdapter returns data on 200

--- test/infra/http/http_adapter_test.dart
+++ test/infra/http/http_adapter_test.dart
@@ -172,5 +172,22 @@ void main() {
         'accept': 'application/json',
       }));
     });
+
+
+    test('Should return data if get returns 200', () async {
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, {'any_key': 'any_value'});
+    });
+
+    test('Should return null if get returns 200 with no data', () async {
+      mockResponse(200, body: "");
+
+      final response = await sut.request(url: url, method: 'get');
+
+      expect(response, null);
+    });
+
+
   });
 }
